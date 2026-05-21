f935737fec83b79fdd1ae3de95893e99a4f55dfe
Harshil Parmar <harshilparmar1326@gmail.com>
2026-05-21 11:31:33 +0530

    fix: remove extra div in HOF

--- src/app/hall-of-fame/page.tsx
+++ src/app/hall-of-fame/page.tsx
@@ -210,7 +210,6 @@ export default function HallOfFamePage() {
                     )}
 
                   </div>
-                  </div>
                 </div>
               </div>
             ))}
